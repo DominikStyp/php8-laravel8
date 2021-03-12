27c7958a430d5db56e1858cb302bb1d33f4e02cc
DominikStyp <dominik.stypula1@gmail.com>
2021-03-12 20:57:29 +0100

    File uploads with mocking disk storage

--- example-app/tests/Feature/FileUploadTest.php
+++ example-app/tests/Feature/FileUploadTest.php
@@ -15,20 +15,41 @@ class FileUploadTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_avatar_upload()
     {
         Storage::fake('images');
 
-        $file = UploadedFile::fake()->image('avatar.jpg');
+        $file = UploadedFile::fake()->image('avatar.jpg', 50, 50)->size(100);
 
         $response = $this->json('POST', '/upload_files', [
             'image' => $file,
         ]);
+        $response->assertSuccessful();
 
         // Assert the file was stored...
         Storage::disk('images')->assertExists("uploaded/".$file->hashName());
 
         // Assert a file does not exist...
         Storage::disk('images')->assertMissing('uploaded/missing.jpg');
+    }
+
+    public function test_pdf_upload()
+    {
+        Storage::fake('images');
+
+        $file = UploadedFile::fake()->create('document.pdf', 111, 'application/pdf');
+
+        $response = $this->json('POST', '/upload_files', [
+            'image' => $file,
+        ]);
+        $response->assertSuccessful();
+
+        // Assert the file was stored...
+        Storage::disk('images')->assertExists("uploaded/".$file->hashName());
+
+        $uploadedSize = Storage::disk('images')
+            ->size("uploaded/".$file->hashName());
+        $this->assertEquals($file->getSize(), $uploadedSize);
+
     }
 }
