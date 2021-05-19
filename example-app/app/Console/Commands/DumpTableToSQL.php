08a4325dfc43da8762aa1078c593709de83f4139
DominikStyp <dominik.stypula1@gmail.com>
2021-05-19 06:36:35 +0200

    Commands: dump & import table from sql file

---
+++ example-app/app/Console/Commands/DumpTableToSQL.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class DumpTableToSQL extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:dump-table-to-sql {table : table to dump}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Dumps table structure and data to SQL';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        $table = $this->argument('table');
+
+        $targetFile = "database/schema/{$table}.sql";
+
+        if(!is_dir($targetDir = dirname($targetFile))){
+            mkdir($targetDir, 0777, true);
+        }
+
+        $this->line("Dumping table {$table}");
+
+        $command = 'mysqldump -u "${:USERNAME}" -h "${:DB_HOST}" -p"${:PASSWORD}" "${:DB_NAME}" "${:TABLE}" > "${:TARGET_FILE}"';
+
+        // we use cross-platform process factory
+        // @see: https://symfony.com/doc/current/components/process.html
+        $process = Process::fromShellCommandline($command, null, [
+            'USERNAME' => config('database.connections.mysql.username'),
+            'PASSWORD' => config('database.connections.mysql.password'),
+            'DB_HOST' => config('database.connections.mysql.host'),
+            'DB_NAME' => config('database.connections.mysql.database'),
+            'TABLE' => $table,
+            'TARGET_FILE' => $targetFile
+        ]);
+
+        $this->line( json_encode($process->getEnv(), JSON_PRETTY_PRINT) );
+
+        $process->run();
+
+        // if something is wrong we throw an exception
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        $this->line($process->getOutput());
+
+        $this->line("table dumped to: $targetFile");
+
+        return 0;
+    }
+}
---
+++ example-app/app/Console/Commands/ImportTableFromSQL.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class ImportTableFromSQL extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:import-table-from-sql {table : table to dump}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Imports table structure with data from sql file';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $table = $this->argument('table');
+
+        $targetFile = "database/schema/{$table}.sql";
+
+        if(!file_exists($targetFile)){
+            throw new \Exception("File {$targetFile} doesn't exist");
+        }
+
+        $this->line("Importing table {$table}");
+
+        $command = 'mysql -u "${:USERNAME}" -h "${:DB_HOST}" -p"${:PASSWORD}" "${:DB_NAME}" < "${:TARGET_FILE}"';
+
+        // we use cross-platform process factory
+        // @see: https://symfony.com/doc/current/components/process.html
+        $process = Process::fromShellCommandline($command, null, [
+            'USERNAME' => config('database.connections.mysql.username'),
+            'PASSWORD' => config('database.connections.mysql.password'),
+            'DB_HOST' => config('database.connections.mysql.host'),
+            'DB_NAME' => config('database.connections.mysql.database'),
+            'TARGET_FILE' => $targetFile
+        ]);
+
+        $this->line( json_encode($process->getEnv(), JSON_PRETTY_PRINT) );
+
+        $process->run();
+
+        // if something is wrong we throw an exception
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        $this->line($process->getOutput());
+
+        $this->line("table imported from: $targetFile");
+
+        return 0;
+    }
+}
